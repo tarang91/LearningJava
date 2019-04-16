429e8166e0f63f8e33afcee41fe22841ba17ffcf
tarangsharma <tarangsharma@ASDW0063.qait.com>
2019-04-16 14:40:27 +0530

    Commit from office

---
+++ LearningJava/src/com/package1/one/Beverage.java
@@ -0,0 +1,5 @@
+package com.package1.one;
+
+public class Beverage {
+
+}
---
+++ LearningJava/src/com/package2/two/Tea.java
@@ -0,0 +1,7 @@
+package com.package2.two;
+import com.package1.one.Beverage;
+public class Tea extends Beverage {
+ public static void main (String args[]) {
+	 System.out.println("Test");
+ }
+}
